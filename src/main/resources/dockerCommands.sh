61ddc4c70eb7c580d0f025fc42d02107cd7a4afa
Suresh Kadthan <skadthan@gmail.com>
2020-05-10 13:11:04 -0400

    Adding JUnit5 test cases, docker swarm commands.

--- src/main/resources/dockerCommands.sh
+++ src/main/resources/dockerCommands.sh
@@ -15,3 +15,13 @@ docker run --name ashu-cassandra -p 9042:9042 -p 9160:9160 --network cassandra-n
 docker exec -it springboot2cassandra bash
 apt-get update
 apt-get install vim
+
+#Docker Swarm
+docker swarm init
+docker node ls
+
+#To add a worker to this swarm, run the following command:
+#docker swarm join --token SWMTKN-1-5541joibfa4cqy1lb3i2m6emhcc76hvrzwjecqm8ikdsufw3ge-51l6y92s7rmackbfezu7dtl2d 192.168.65.3:2377
+#To add a manager to this swarm, run 'docker swarm join-token manager' and follow the instructions
+
+docker service create --name springboot2cassandra --replicas 2 -p 8080:8080 skadthan/springboot2cassandra:v1
---
+++ src/test/java/com/app/harmony2/restcontroller/DispositionControllerTest.java
@@ -0,0 +1,93 @@
+package com.app.harmony2.restcontroller;
+
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+import com.harmony2.app.TargetingHistoryApplication;
+import com.harmony2.app.model.DispositionRecord;
+import com.harmony2.app.service.DispositionRecordService;
+
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//import java.time.Clock;
+//import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes={TargetingHistoryApplication.class})
+@AutoConfigureMockMvc
+public class DispositionControllerTest {
+
+	@Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    private DispositionRecordService DispositionService;
+    
+    
+    @Test
+    void getDispositionData() throws Exception {
+    	
+    	
+		/*
+		 * Clock c = Clock.systemUTC(); Duration d = Duration.ofHours(5); Clock clock =
+		 * Clock.offset(c, d); System.out.println(clock.instant());
+		 * Instant timestamp = clock.instant();
+		 */
+    	Instant timestamp = Instant.now();
+    	String recordtime = timestamp.toString();
+    	System.out.println(recordtime);
+
+    	String ssoid = "2434bb6a-a92c-43e9-8d29-6c8d690ac7c2";
+    	
+    	DispositionRecord dispositionRecord = new DispositionRecord();
+    	dispositionRecord.setSsoid(ssoid);
+    	dispositionRecord.setDisptype("Loved");
+    	dispositionRecord.setLayout("L1");
+    	dispositionRecord.setOffername("AshuOffer");
+    	dispositionRecord.setPlacement("AccountMessage");
+    	dispositionRecord.setRecordtime(timestamp);
+    	dispositionRecord.setStyle("S1");
+    
+    	List<DispositionRecord> dispositioRecords = new ArrayList<DispositionRecord>();
+    	dispositioRecords.add(dispositionRecord);
+    	when(DispositionService.getDispositionRecord(ssoid)).thenReturn(dispositioRecords);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/getdispositiondata")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"ssoid\": \"2434bb6a-a92c-43e9-8d29-6c8d690ac7c2\"}") 
+                .accept(MediaType.APPLICATION_JSON)
+        )
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+        .andExpect(jsonPath("$[0].ssoid").value("2434bb6a-a92c-43e9-8d29-6c8d690ac7c2"))
+        .andExpect(jsonPath("$[0].offername").value("AshuOffer"))
+        .andExpect(jsonPath("$[0].placement").value("AccountMessage"))
+        .andExpect(jsonPath("$[0].layout").value("L1"))
+        .andExpect(jsonPath("$[0].style").value("S1"))
+        .andExpect(jsonPath("$[0].disptype").value("Loved"))
+        .andExpect(jsonPath("$[0].recordtime").value(recordtime))
+        .andDo(print());
+    }
+
+}
